f8f3f6146acfff94662d519e88db534647a8a9cb
geekceo04 <120865810+geekceo04@users.noreply.github.com>
2022-12-29 22:00:40 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-setTimeout(() => location.href = "main.html", 3000);
+while (1){
+    password = prompt("비밀번호를 입력하세요.");
+    if (password == 1) {
+        setTimeout(() => location.href = "main.html", 3000);
+        break
+    }
+    else {
+        alert("비밀번호가 틀렸습니다.")
+    }
+}
